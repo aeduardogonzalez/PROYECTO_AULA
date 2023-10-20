0159b5ead181f2f0199789ccdf35a59d75d8f841
JJCO10 <juanjosecarmonaortiz@gmail.com>
2023-10-20 16:08:20 -0500

    presentacion GUI

--- GUI/frmSession.Designer.cs
+++ GUI/frmSession.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.btnCrearSesion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Bell MT", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.Location = new System.Drawing.Point(151, 123);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(201, 95);
+            this.btnIniciarSesion.TabIndex = 0;
+            this.btnIniciarSesion.Text = "iniciar sesion";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCrearSesion
+            // 
+            this.btnCrearSesion.Font = new System.Drawing.Font("Bell MT", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrearSesion.Location = new System.Drawing.Point(151, 279);
+            this.btnCrearSesion.Name = "btnCrearSesion";
+            this.btnCrearSesion.Size = new System.Drawing.Size(201, 95);
+            this.btnCrearSesion.TabIndex = 1;
+            this.btnCrearSesion.Text = "crear sesion";
+            this.btnCrearSesion.UseVisualStyleBackColor = true;
+            // 
+            // frmSession
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(502, 490);
+            this.Controls.Add(this.btnCrearSesion);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmSession";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Button btnCrearSesion;
     }
 }
 
