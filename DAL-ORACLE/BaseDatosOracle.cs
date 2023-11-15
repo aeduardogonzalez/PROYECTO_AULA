11e500e644a6e3195125ae30037b1a6d195d70b9
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2023-11-15 09:54:28 -0500

    Conexion exitosa a la base de datos

--- DAL-ORACLE/BaseDatosOracle.cs
+++ DAL-ORACLE/BaseDatosOracle.cs
@@ -10,6 +10,26 @@ namespace DAL_ORACLE
 {
     public class BaseDatosOracle
     {
-        
+        protected OracleConnection conexion = null;    // = new OracleConnection();
+        protected string cadena = "user id=USER1;password=123;data source=" +
+                                "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)" +
+                                "(HOST=localhost)(PORT=1521))(CONNECT_DATA=" +
+                                "(SERVICE_NAME=XE)))";
+
+        public BaseDatosOracle()
+        {
+            conexion = new OracleConnection(cadena);
+        }
+        public string AbrirConexion()
+        {
+            conexion.Open();
+            return conexion.State.ToString();
+        }
+
+        public string CerrarConexion()
+        {
+            conexion.Close();
+            return conexion.State.ToString();
+        }
     }
 }
