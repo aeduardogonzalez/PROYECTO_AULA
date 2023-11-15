af175ab7f6df741bab190a1f8057f10e06a5a257
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2023-11-15 07:20:42 -0500

    Intento de Conección a la Base de Datos

---
+++ DAL-ORACLE/PrestamistaRepositoryOracle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITY;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+
+namespace DAL_ORACLE
+{
+    internal class PrestamistaRepositoryOracle : BaseDatosOracle
+    {
+
+    }
+}
