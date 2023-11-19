29c3c1d3cd863833cc4f15ee4b272398af3c3d8d
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2023-11-19 10:17:01 -0500

    Actualización GUI

--- GUI/frmAñadirCliente.cs
+++ GUI/frmAñadirCliente.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ENTITY;
 namespace GUI
 {
     public partial class frmAñadirCliente : Form
