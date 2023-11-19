9db044cfbf5291df0d9f8d499cf68a72bb7f0231
JJCO10 <juanjosecarmonaortiz@gmail.com>
2023-11-19 09:04:36 -0500

    actuliazacion GUI, frmSession

--- GUI/frmSession.Designer.cs
+++ GUI/frmSession.Designer.cs
@@ -28,43 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSession));
             this.btnIniciarSesion = new System.Windows.Forms.Button();
             this.btnCrearSesion = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnIniciarSesion
             // 
-            this.btnIniciarSesion.Font = new System.Drawing.Font("Bell MT", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarSesion.Location = new System.Drawing.Point(266, 53);
+            this.btnIniciarSesion.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Bell MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(251, 88);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
             this.btnIniciarSesion.Size = new System.Drawing.Size(201, 61);
             this.btnIniciarSesion.TabIndex = 0;
             this.btnIniciarSesion.Text = "iniciar sesion";
-            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
             this.btnIniciarSesion.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnCrearSesion
             // 
+            this.btnCrearSesion.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnCrearSesion.Font = new System.Drawing.Font("Bell MT", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCrearSesion.Location = new System.Drawing.Point(266, 165);
+            this.btnCrearSesion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCrearSesion.Location = new System.Drawing.Point(251, 177);
             this.btnCrearSesion.Name = "btnCrearSesion";
             this.btnCrearSesion.Size = new System.Drawing.Size(201, 61);
             this.btnCrearSesion.TabIndex = 1;
             this.btnCrearSesion.Text = "crear sesion";
-            this.btnCrearSesion.UseVisualStyleBackColor = true;
+            this.btnCrearSesion.UseVisualStyleBackColor = false;
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btnCrearSesion);
             this.groupBox1.Controls.Add(this.btnIniciarSesion);
             this.groupBox1.Location = new System.Drawing.Point(4, 6);
@@ -75,14 +82,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(24, 206);
+            this.textBox2.Location = new System.Drawing.Point(24, 218);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(189, 20);
             this.textBox2.TabIndex = 5;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 94);
+            this.textBox1.Location = new System.Drawing.Point(24, 155);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(189, 20);
             this.textBox1.TabIndex = 4;
@@ -91,22 +98,22 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bell MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 175);
+            this.label2.Location = new System.Drawing.Point(19, 187);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 27);
             this.label2.TabIndex = 3;
             this.label2.Text = "Contraseña";
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bell MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 27);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Usuario";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(24, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // frmSession
             // 
@@ -119,6 +126,7 @@
             this.Text = "CrediXpert";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -128,10 +136,10 @@
         private System.Windows.Forms.Button btnIniciarSesion;
         private System.Windows.Forms.Button btnCrearSesion;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
